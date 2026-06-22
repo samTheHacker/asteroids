57bc1b9205f4b94e0eb0ccab019b7496877a932e
samTheHacker <mobilvodoo@googlemail.com>
2026-06-22 22:45:28 +0100

    Add basic Gameloop logic Draw the game to the screen. Add .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pygame
+import constants
+from logger import log_state
 
 def main():
+    pygame.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
