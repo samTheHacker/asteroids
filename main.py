25d5ff14bbec3a40078db15373a3cbc7bf274352
samTheHacker <mobilvodoo@googlemail.com>
2026-06-27 14:12:35 +0100

    Add shooting and limiters

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField 
+from shot import Shot 
 from logger import log_state, log_event
 import sys 
 
@@ -10,12 +11,20 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0.0
+    
+    # Groups
     updatable  = pygame.sprite.Group()
     drawable  = pygame.sprite.Group()
     asteroids  = pygame.sprite.Group()
+    shots  = pygame.sprite.Group()
+    
+    # Containers
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
+    #Game logic
     AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 from circleshape import CircleShape
+from shot import Shot
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 
 class Player(CircleShape):
     
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
         
     # in the Player class
     def triangle(self) -> list[pygame.Vector2]:
@@ -25,7 +27,9 @@ class Player(CircleShape):
         
     def update(self, dt: float) -> None:
         keys = pygame.key.get_pressed()
+        self.shot_cooldown -= dt
 
+        #move
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
@@ -33,10 +37,26 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_w]:
-            self.move(dt)                 
+            self.move(dt) 
+
+        #shoot
+        if keys[pygame.K_SPACE]:
+            self.shoot()                     
     
     def move(self, dt):
         unit_vector = pygame.Vector2(0, 1)
         rotated_vector = unit_vector.rotate(self.rotation)
         rotated_with_speed_vector = rotated_vector * PLAYER_SPEED * dt
         self.position += rotated_with_speed_vector
+        
+    def shoot(self):
+        if self.shot_cooldown > 0:
+           pass
+        else:
+            self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+            shoot = Shot(self.position.x, self.position.y)
+            shoot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            
+               
+   
+        
